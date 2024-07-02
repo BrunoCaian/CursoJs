1d2b0d3b747fc529821abb0e3e3650af95db0e6e
BrunoCaian <brunocaian81@gmail.com>
2024-07-01 22:05:03 -0300

    exercicios

---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,7 @@
+var vel = 89
+
+if (vel > 100) {
+    console.log(`Sua velocidade é de ${Number}KM/h, acima do limite permitido, portando será multado!`)
+}else {
+    console.log(`Parabéns, sua velocidade é de ${vel}, dentro do limite permitido!`)
+}
