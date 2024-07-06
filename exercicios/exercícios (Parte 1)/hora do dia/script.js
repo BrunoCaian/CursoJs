f872a805a8f5afefb2354e2d2490b951c5334359
BrunoCaian <brunocaian81@gmail.com>
2024-07-06 19:16:42 -0300

    hora do dia

--- exercicios/exercícios (Parte 1)/hora do dia/script.js
+++ exercicios/exercícios (Parte 1)/hora do dia/script.js
@@ -1,22 +1,21 @@
+function carregar() {
 var agora = new Date()
 var hora = agora.getHours()
 var minutos = agora.getMinutes()
 var hora_atual = window.document.getElementById('hora')
 var cor_fundo = window.document.getElementById('fundo')
-var dia = window.document.getElementById('dia')
-var tarde = window.document.getElementById('tarde')
-var noite = window.document.getElementById('noite')
     hora_atual.innerText = `Agora são ${hora} Horas e ${minutos} minutos.`
 
     if (hora >= 5 && hora < 12) {
         dia.style.display = 'block'
-        cor_fundo.style.backgroundColor ='rgb(235, 198, 154)' 
+        cor_fundo.style.backgroundColor ='#e2cd9f' 
 
     } else if (hora >= 12 && hora < 18) {
         tarde.style.display = 'block'
-        cor_fundo.style.backgroundColor = 'lightsalmon'
+        cor_fundo.style.backgroundColor = '#b9846f'
     } else {
         noite.style.display = 'block'
-        cor_fundo.style.backgroundColor = 'rgba(0, 0, 0, 0.721'
+        cor_fundo.style.backgroundColor = '#515154'
 
     }
+}    
